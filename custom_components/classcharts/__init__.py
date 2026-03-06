d36abc76af7c5d41644519ef3141f6a24e4f8902
skid88 <155565071+skid88@users.noreply.github.com>
2026-03-06 20:15:10 +0000

    Update __init__.py

--- custom_components/classcharts/__init__.py
+++ custom_components/classcharts/__init__.py
@@ -32,7 +32,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     hass.data.setdefault(DOMAIN, {})
     hass.data[DOMAIN][entry.entry_id] = coordinator
 
-    await hass.config_entries.async_forward_entry_setups(entry, ["calendar", "sensor"])
+    await hass.config_entries.async_forward_entry_setups(entry, ["calendar_", "sensor"])
     
     entry.async_on_unload(entry.add_update_listener(update_listener))
     
